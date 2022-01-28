b9ba178bb127b6606edff379f3639d8e14584d9e
ParthibanPARTHIE <98578936+ParthibanPARTHIE@users.noreply.github.com>
2022-01-28 19:23:59 +0530

    Add files via upload

---
+++ Charoccurance.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+public class Charoccurance {
+	public static void main (String[]args) {
+		String str = "welcome to chennai ";
+		char search = 'c';
+		int count =0;
+		for (int i =0; i<str.length(); i++) {
+			if (search == str.charAt(i))
+				count ++;
+		}
+		System.out.println("count of"  +   search + "is"  +   count);
+		
+		
+		
+		
+		
+	}
+
+}
